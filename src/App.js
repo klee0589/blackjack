47d51c2c4f8b6b0cb649f836c8761f59ed116ee8
Daniel <klee0589@gmail.com>
2024-01-24 20:48:13 -0500

    move all to api folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import BlackJackTable from './components/BlackJack/BlackJackTable';
+import { getNewDeck } from './utils/api';
 import './App.css';
-
-async function getNewDeck() {
-  const response = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=8');
-  if (!response.ok) {
-    throw new Error(`Error: ${response}`)
-  }
-  const result = await response.json();
-  return result.deck_id
-}
 
 function App() {
   const [deckId, setDeckId] = useState(null);
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,4 +1,13 @@
 
+export async function getNewDeck() {
+    const response = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=8');
+    if (!response.ok) {
+        throw new Error(`Error: ${response}`)
+    }
+    const result = await response.json();
+    return result.deck_id
+}
+
 export async function getTwoCards(deck_id) {
     const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`);
     if (!response.ok) {
